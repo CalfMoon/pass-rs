f460172ed6df520ee6849a4fc33c0ecb53089c2b
Kritagya Bhattarai (CalfMoon) <kritagyabhattarai@proton.me>
2024-08-04 18:13:03 +0545

    feat: add clap for parsing flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Test argument
+    #[arg(short, long)]
+    test: String,
+}
+
 fn main() {
-    println!("Hello World!");
+    let args = Args::parse();
+
+    println!("{}", args.test);
 }
