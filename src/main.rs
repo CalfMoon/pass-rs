48c7b62241c55830e823641db25466568cc81eb8
Kritagya Bhattarai (CalfMoon) <kritagyabhattarai@proton.me>
2024-08-05 21:11:09 +0545

    feat: add method that gets path of store

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,23 @@ enum SubCommand {
     },
 }
 
+impl Arg {
+    fn get_path(&self) -> PathBuf {
+        let mut return_path = PathBuf::new();
+        match &self.subcommand {
+            SubCommand::Init { gpg_id: _, path } => match path {
+                Some(x) => return_path.push(x),
+                None => return_path.push("~/.local/share/"),
+            },
+            _ => unreachable!(),
+        }
+
+        return_path.push("rs-passstore");
+        return_path
+    }
+}
+
 fn main() {
     let args = Arg::parse();
+    println!("{:?}", args.get_path());
 }
